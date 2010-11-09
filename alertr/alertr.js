0caccb60f6a5647c0941f32752a140b5c969375c
Leon Barrett <leon@leonbarrett.com>
2010-11-09 16:57:02 +0000

    first commit

---
+++ alertr/alertr.js
@@ -0,0 +1,37 @@
+﻿(function($){
+
+$.fn.alertr = function(alertDuration){
+
+
+
+
+var element = this;
+
+			if($(element).length)
+			{
+			$(element).fadeIn("fast");	
+				
+				
+				if(alertDuration){
+
+
+var alerttimer = window.setTimeout(function () {
+					$(element).trigger('click');
+				}, alertDuration);
+}
+				
+				
+				
+				
+		
+				
+				$(element).click(function () {
+					window.clearTimeout(alerttimer);
+					$(element).fadeOut("slow");					
+				});
+			}
+
+};
+
+
+})(jQuery);
